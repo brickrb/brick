4f290862742a8c411842bcdad7969ec8a33b94e4
Jon Moss <me@jonathanmoss.me>
2015-03-28 21:25:02 -0400

    [Config] Working!

--- lib/brick/config.rb
+++ lib/brick/config.rb
@@ -1,6 +1,46 @@
+require 'pathname'
+
 module Brick
   # This is the storage location for Brick's global configuration.
 
   class Config
+    def self.instance
+      @instance ||= new
+    end
+
+    def self.instance=(instance)
+      @instance = instance
+    end
+
+    attr_accessor :repos_dir, :verbose, :silent
+    alias_method :clean?,   :clean
+    alias_method :verbose?, :verbose
+    alias_method :silent?,  :silent
+
+    def initialize
+      @repos_dir = Pathname.new(File.expand_path("~/.bricks"))
+      @verbose = false
+      @silent = false
+    end
+
+    def project_root
+      Pathname.pwd
+    end
+
+    def project_brickfile
+      unless @project_brickfile
+        @project_brickfile = project_root + 'Brickfile'
+        unless @project_brickfile.exist?
+          @project_brickfile = project_root.glob('*.brickspec').first
+        end
+      end
+      @project_brickfile
+    end
+
+    module Mixin
+      def config
+        Config.instance
+      end
+    end
   end
 end
