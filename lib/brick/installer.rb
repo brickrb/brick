382b0772996a47cd5fad9b554a480243cecd3258
Jon Moss <me@jonathanmoss.me>
2015-04-21 17:42:56 -0400

    [Installer] Basic outline

--- lib/brick/installer.rb
+++ lib/brick/installer.rb
@@ -1,4 +1,67 @@
 module Brick
   class Installer
+    include Config::Mixin
+
+    # @return [Brickfile] The Brickfile specification that contains the information
+    #         of the Bricks that should be installed.
+    #
+    attr_reader :brickfile
+
+    # @param  [Brickfile]  brickfile     @see brickfile
+    #
+    def initialize(brickfile)
+      @brickfile  = brickfile
+    end
+
+    # Install the Bricks!
+    #
+    # The installation process of is mostly linear with few minor complications
+    # to keep in mind:
+    #
+    # @return [void]
+    #
+    def install!
+      # Resolve Dependencies
+        UI.puts "Analyzing dependencies"
+        analyze
+
+      # Download Dependencies
+        UI.puts "Downloading dependencies"
+        install_brick_sources
+    end
+
+    #-------------------------------------------------------------------------#
+
+    public
+
+    # @!group Installation results
+
+    # @return [Array<String>] The Bricks that should be installed.
+    #
+    attr_reader :names_of_bricks_to_install
+
+    # @return [Array<Specification>] The specifications that where installed.
+    #
+    attr_accessor :installed_specs
+
+    #-------------------------------------------------------------------------#
+
+    private
+
+    # @!group Installation steps
+
+    # @return [void]
+    #
+    def analyze
+    end
+
+    # Downloads, installs the documentation and cleans the sources of the Bricks
+    # which need to be installed.
+    #
+    # @return [void]
+    #
+    def install_brick_sources
+    end
+
   end
 end
